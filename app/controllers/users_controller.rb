114ce935e4878e9c4d3c85b09ff4307f6fbfc36c
Jugal kishor <jugalpatel854@gmail.com>
2022-10-12 18:59:49 +0530

    submit form by ajax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,24 +11,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def add_friends
-    # if params[:search].present?
-    #   friends_ids = FriendShip.where('user_id= ? OR friend_id= ?', current_user.id, current_user.id)
-    #   @result = User.where.not(id: friends_ids.pluck(:user_id, :friend_id).flatten)
-    #   @users = @result.search(params[:search])
-    # end
-      
-      friends_ids = FriendShip.where('user_id= ? OR friend_id= ?', current_user.id, current_user.id)
-      @users = User.where.not(id: friends_ids.pluck(:user_id, :friend_id).flatten)
+  def add_friends      
+    friends_ids = FriendShip.where('user_id= ? OR friend_id= ?', current_user.id, current_user.id)
+    @users = User.where.not(id: friends_ids.pluck(:user_id, :friend_id).flatten)
   end
 
   def friends
-    if params[:search].present?
-      @res = FriendShip.where("friend_id IN (:user) OR user_id IN (:user)", {user: current_user.id}).where(status: true).includes(:user, :friend)
-      @users = @res.search(params[:search])      
-    else
-      @users = FriendShip.where("friend_id IN (:user) OR user_id IN (:user)", {user: current_user.id}).where(status: true).includes(:user, :friend)
-    end
+    @users = FriendShip.where("friend_id IN (:user) OR user_id IN (:user)", {user: current_user.id}).where(status: true).includes(:user, :friend)
   end
 
   def requests
@@ -43,16 +32,22 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success]="Registered successfully"
-      redirect_to new_session_path
-    else  
-      render 'new'
+      respond_to do |format|
+        format.html {redirect_to new_session_path}
+        format.js
+      end
     end
   end
 
   def send_request
-    FriendShip.create(user_id: current_user.id, friend_id: params[:user_id], status: false)
-    redirect_to users_add_friends_path
+    # debugger
+    @user = FriendShip.create(user_id: current_user.id, friend_id: params[:user_id], status: false)
+    @user_id = params[:user_id]
+    @fr_id = params[:id]
+      respond_to do |format|
+        format.html {redirect_to users_add_friends_path}
+        format.js
+      end
   end
 
   def accept_request
@@ -62,11 +57,18 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @fr_id = params[:id]
+    @user_id = params[:user_id]
+    #debugger
+    #@friend_id = params[:friend_id]
     @unfriend = FriendShip.find(params[:id])
     if @unfriend.present?
       @unfriend.destroy
     end
-    redirect_to friends_path
+    respond_to do |format|
+        format.html {redirect_to friends_path}
+        format.js
+      end
   end
 
   private
