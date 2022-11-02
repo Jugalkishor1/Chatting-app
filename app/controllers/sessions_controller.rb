d496d28a05beca462aef427e530f94fd44d4042c
Jugal kishor <jugalpatel854@gmail.com>
2022-11-02 12:25:40 +0530

    add validation on register and show flash message on succesfull or failed login and register

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,20 +7,21 @@ class SessionsController < ApplicationController
   def create
    @user = User.find_by(email: params[:email])
        # debugger
-       if @user.present?
-        if @user.password == params[:password]
-          session[:user_id] = @user.id
-          redirect_to '/users/dashboard'
-        else
-          flash.now[:notice] = "Incorrect Password"
-          render 'new'
-        end
-      else        
-        flash.now[:notice] = "Incorrect Email" 
+    if @user.present?
+      if @user.password == params[:password]
+        session[:user_id] = @user.id
+        redirect_to '/users/dashboard'
+      else
+        flash.keep[:notice] = "Incorrect Password"
+        redirect_to new_session_path
       end
+    else        
+      redirect_to new_session_path
+      flash.keep[:notice] = "Incorrect Email" 
     end
+  end
 
-    def destroy
+  def destroy
     # debugger
     session.delete(:user_id)
     @current_user = nil
