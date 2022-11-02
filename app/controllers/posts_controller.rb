82cc336da236ab63b27411dea9a6055181f7682e
Jugal kishor <jugalpatel854@gmail.com>
2022-11-02 12:27:45 +0530

    only current user can edit their posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :require_login
   def index
     @posts = Post.all 
     respond_to do |format|
@@ -7,12 +8,17 @@ class PostsController < ApplicationController
     end
   end
 
-
   def show
-    @posts = Post.all
+    if @post.present?
+      @post = current_user.posts
+    else
+      flash[:alert] = "Post not found"
+      redirect_to profile_path
+    end
   end
 
-  def new
+  def profile
+    @posts = Post.where(user_id: current_user.id)
   end
   
   def create
@@ -21,6 +27,19 @@ class PostsController < ApplicationController
 
     if @post.save
       redirect_to posts_path
+    end
+  end
+
+  def edit    
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to profile_path
+    else
+      render :edit
     end
   end
 
