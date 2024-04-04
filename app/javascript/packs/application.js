c01017853b77a828f818a7bf8646adb0b9d554b3
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2024-04-04 13:53:02 +0530

    Remove CDN links of bootstrap and jquery and add gem and depedencies for that

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled. 
 
+//= require jquery
+//= require jquery_ujs
+//= require popper
+//= require turbolinks
+//= require bootstrap
+//= require_tree.
+
 // Import dependencies
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -10,16 +17,16 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 // Import jQuery and set it globally
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
 import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
 
-// Import Bootstrap JavaScript components
+// // Import Bootstrap JavaScript components
 import 'bootstrap';
-// import '@popperjs/core';
-import "bootstrap/scss/bootstrap";
-import 'bootstrap/js/dist/modal';
+// // import '@popperjs/core';
+// import "bootstrap/scss/bootstrap";
+// import 'bootstrap/js/dist/modal';
 
 require("trix")
 require("@rails/actiontext")
