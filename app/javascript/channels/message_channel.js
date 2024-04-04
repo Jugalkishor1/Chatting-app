a5247a7d58468dd5e28af6091e0b08848764912d
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2024-04-04 17:12:25 +0530

    Fix some error related to action cable

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -1,8 +1,7 @@
 import consumer from "./consumer"
 
 document.addEventListener('turbolinks:load', () => {
-  const group_id = document.getElementById("group_id").value;
-  // debugger;
+  const group_id = document.getElementById("group_id")?.value;
   consumer.subscriptions.create({channel: "MessageChannel", group_id: group_id}, {
     connected() {
       // Called when the subscription is ready for use on the server
@@ -14,16 +13,20 @@ document.addEventListener('turbolinks:load', () => {
     },
 
     received(data) {
-      // Called when there's incoming data on the websocket for this channel
-      console.log(data)
-      var g_id = data.message.group_id
-      var currentUser = $(".body-section").attr("data-current");
-      var className = ((parseInt(currentUser) == parseInt(data.message.sender_id)) ? "msg-right" : "msg-left")
-      var mainData = "<div class='msg-left-sub msg-desc'><div><h5>"+data.message.m_body+"<br><span style='float:right;'> :-"+data.current_user+"</span><br></h5></div></div>"
-      
-      $(".chat-template.group-" + g_id + " ul").append("<li class="+className+">"+mainData+"</li>");
-      // $("#new_message").append("<h3 class='msg-left-sub msg-desc'>" + data.current_user + ":- " + data.message.m_body + "</h3>");
-      
+      console.log(data);
+      if (data.message) {
+        var g_id = data.message.group_id;
+        var currentUser = $(".body-section").attr("data-current");
+        var className = ((parseInt(currentUser) == parseInt(data.message.sender_id)) ? "msg-right" : "msg-left");
+        var mainData = "<div class='msg-left-sub msg-desc'><div><h5>"+data.message.m_body+"<br><span style='float:right;'> :-"+data.current_user+"</span><br></h5></div></div>";
+        
+        $(".chat-template.group-" + g_id + " ul").append("<li class="+className+">"+mainData+"</li>");
+      } else if (data.html) {
+        // Handle HTML content directly or based on your application logic
+        $(".chat-tesdmplate").append(data.html);
+      } else {
+        console.error("Received message data does not contain 'message' or 'html' property:", data);
+      }
     }
   });
 });
